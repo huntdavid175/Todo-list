374cc8d28d643e3ab10a30ec0c5a5e501dd8a959
agent14k <huntdavid175@gmail.com>
2020-09-26 23:05:44 +0000

    installed required modules needed for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    }
-}
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
